8d1dace12b2d1d00a0426236194e54526b73917a
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2025-12-06 15:32:34 +0200

    registration and login

--- defeat_academy/settings.py
+++ defeat_academy/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'academy',
+    'users',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -111,7 +112,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+# LOGIN_REDIRECT_URL = 'academy:index'
+# LOGOUT_REDIRECT_URL = 'academy:index'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
--- defeat_academy/urls.py
+++ defeat_academy/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('academy.urls'))
+    path('', include('academy.urls')),
+    path('users/', include('users.urls')),
 ]
